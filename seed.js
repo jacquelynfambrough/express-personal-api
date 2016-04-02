2fa75a8c4e052b8c93fdaeadb834b6f9cd036fa4
YOUR_GITHUB_USERNAME <jacquelynfambrough@gmail.com>
2016-04-02 12:07:52 -0700

    hardcoded data about myself in seed.js

--- seed.js
+++ seed.js
@@ -1,15 +1,29 @@
 // This file allows us to seed our application with data
 // simply run: `node seed.js` from the root of this project folder.
 
-// var db = require('./models');
+var db = require('./models');
 
-// var new_campsite = {description: "Sharp rocks. Middle of nowhere."}
+var aboutMe = {
+  name: "Jackie Fambrough"
+  github_link: "",
+  github_profile_image:"",
+  pets:[{
+    name:"Antonio",
+    type:"dog",
+    alive:true
+  },
+  {
+    name:"Beatrice",
+    type:"cat",
+    alive:false
+  }]
+};
 
-// db.Campsite.create(new_campsite, function(err, campsite){
-//   if (err){
-//     return console.log("Error:", err);
-//   }
+db.Campsite.create(new_campsite, function(err, campsite){
+  if (err){
+    return console.log("Error:", err);
+  }
 
-//   console.log("Created new campsite", campsite._id)
-//   process.exit(); // we're all done! Exit the program.
-// })
+  console.log("Created new campsite", campsite._id)
+  process.exit(); // we're all done! Exit the program.
+})
